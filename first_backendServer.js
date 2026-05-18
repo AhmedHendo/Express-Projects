f1ac466d0623c5ee93a24809fb628d19b177c344
AhmedHendo <ahmedhendo2003@gmail.com>
2026-05-18 21:57:14 +0300

    Delete first_backendServer.js

--- first_backendServer.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"); // Import the Express library to create a web server
-const app = express(); // Create an instance of the Express application (server)
-
-app.get("/", (req, res) => { // Register a route for the root URL ("/") "HomePage" that listens for GET requests
-  res.send("Hello World");
-});
-
-app.listen(3000); // Start the server and have it listen on port 3000 for incoming requests
